88501cfceaf40ac6b175afb56a4f4406317b3a1a
Nam Pham <nam.phho@gmail.com>
2020-05-03 15:30:57 +0700

    final starter commit

--- app/src/main/java/com/hnam/recyclerview_week3/MainActivity.kt
+++ app/src/main/java/com/hnam/recyclerview_week3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hnam.recyclerview_week3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun startProfileScreen(idol: Idol){
+        val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+        intent.putExtra(IDOL_NAME_KEY, idol.name)
+        intent.putExtra(IDOL_AVATAR_KEY, idol.avatar)
+        intent.putExtra(IDOL_JOB_KEY, idol.job)
+        startActivity(intent)
     }
 }
